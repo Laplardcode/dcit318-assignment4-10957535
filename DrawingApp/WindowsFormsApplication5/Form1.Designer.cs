fb5ab5eb4382c3b5cf20b4db244a17316f9d7174
Mensah Kwadwo Ofori <komensah005@st.ug.edu.gh>
2024-07-09 20:19:26 +0000

    Drawing App

---
+++ DrawingApp/WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,22 @@
+﻿namespace DrawingApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Drawing App";
+        }
+    }
+}
---
+++ DrawingApp/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DrawingApp
+{
+    public partial class Form1 : Form
+    {
+        private Point? previousPoint = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            previousPoint = e.Location;
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            previousPoint = null;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (previousPoint != null && e.Button == MouseButtons.Left)
+            {
+                using (Graphics g = CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, previousPoint.Value, e.Location);
+                }
+                previousPoint = e.Location;
+            }
+        }
+    }
+}
