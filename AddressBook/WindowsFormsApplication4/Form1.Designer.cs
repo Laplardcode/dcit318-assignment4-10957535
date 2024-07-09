18b124594af8af40cfbd837df3501e0b97906bc8
Mensah Kwadwo Ofori <komensah005@st.ug.edu.gh>
2024-07-09 20:18:00 +0000

    Addressbook

---
+++ AddressBook/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace AddressBook
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label phoneNumberLabel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.phoneNumberLabel = new System.Windows.Forms.Label();
+
+            this.nameTextBox.Location = new System.Drawing.Point(50, 70);
+            this.emailTextBox.Location = new System.Drawing.Point(50, 120);
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(50, 170);
+            this.saveButton.Location = new System.Drawing.Point(50, 220);
+            this.nameLabel.Location = new System.Drawing.Point(50, 50);
+            this.emailLabel.Location = new System.Drawing.Point(50, 100);
+            this.phoneNumberLabel.Location = new System.Drawing.Point(50, 150);
+
+            this.nameLabel.Text = "Name";
+            this.emailLabel.Text = "Email";
+            this.phoneNumberLabel.Text = "Phone Number";
+
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.phoneNumberLabel);
+
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Address Book";
+        }
+    }
+}
---
+++ AddressBook/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            string email = emailTextBox.Text;
+            string phoneNumber = phoneNumberTextBox.Text;
+
+
+
+        }
+    }
+}
+
